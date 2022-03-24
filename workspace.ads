fa21fd2c5f84652d58bebf75d1ec05395b1bc473
Gabriel Cruz <gabrielmoraes@usp.br>
2022-03-24 18:00:08 -0300

    first commit

---
+++ workspace.ads
@@ -0,0 +1,38 @@
+<ADSWorkspace Revision="5" Version="100">
+    <Workspace Name="">
+        <LibraryDefs Name="lib.defs" />
+        <Library Name="ads_standard_layers" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_builtin" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_pelib" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <Library Name="PSI3581_lib" />
+        <Folder Name="aula_1">
+            <Cell Name="PSI3581_lib:filtro_LC" />
+            <Data_Display Name="filtro_LC.dds" />
+        </Folder>
+        <Log Name="search_history.log" />
+        <Log Name="netlist.log" />
+        <Dataset Name="filtro_LC.ds" />
+        <Data_Files Name="filtro_LC.ds" />
+        <Data_Files Name="filtro_LC_data\logFile.txt" />
+        <ConfigFile Name="dds.cfg" />
+    </Workspace>
+</ADSWorkspace>
